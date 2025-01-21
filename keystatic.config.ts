3298f5c7abcc386df4f83e6f3bcc3ddd98e38422
Ricardo Carmo <rcarmo@icapitalnetwork.com>
2025-01-21 19:50:19 +0000

    initial commit

---
+++ keystatic.config.ts
@@ -0,0 +1,26 @@
+import { config, collection, fields } from '@keystatic/core';
+
+export const markdocConfig = fields.markdoc.createMarkdocConfig({});
+
+export default config({
+  storage: {
+    kind: 'github',
+    repo: {
+      owner: "Rick2323",
+      name: "keystatic-test"
+    }  
+    // kind: "local"
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({ label: 'Content' }),
+      },
+    }),
+  },
+});
